7e91fad129828af10d3b635456073fe3c0b6e7a1
Миша <misha.safin.sn0wled@gmail.com>
2021-08-13 18:23:31 +0500

    Решил задание 00_distance.py. Составил словарь словарей расстояний между городами Москва, Лондон и Париж

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 # Есть словарь координат городов
+from pprint import pprint
 
 sites = {
     'Moscow': (550, 370),
@@ -10,13 +8,27 @@ sites = {
 }
 
 # Составим словарь словарей расстояний между ними
+
+distances = {
+    'Moscow': {},
+    'London': {},
+    'Paris': {}
+}
+
+Moscow = sites['Moscow']
+London = sites['London']
+Paris = sites['Paris']
+
+Moscow_London = round(((Moscow[0]-London[0])**2+(Moscow[1]-London[1])**2)**.5,2)
+Moscow_Paris = round(((Moscow[0]-Paris[0])**2+(Moscow[1]-Paris[1])**2)**.5,2)
+London_Paris = round(((London[0]-Paris[0])**2+(London[1]-Paris[1])**2)**.5,2)
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+distances['Moscow'] = {'London': Moscow_London, 'Paris': Moscow_Paris}
+distances['London'] = {'Moscow': Moscow_London, 'Paris': London_Paris}
+distances['Paris'] = {'Moscow': Moscow_Paris, 'London': London_Paris}
 
-# TODO здесь заполнение словаря
-
-print(distances)
+pprint(distances)
 
 
 
