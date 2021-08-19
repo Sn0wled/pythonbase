694f9e805c6d81f9a4ff6f3143b974ecc944bb2a
Миша <misha.safin.sn0wled@gmail.com>
2021-08-19 20:43:31 +0500

    Нарисовал деревья там и тд 04_fractal.py

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import simple_draw as sd
 
+
 # Добавить цвет в функции рисования геом. фигур. из упр lesson_004/01_shapes.py
 # (код функций скопировать сюда и изменить)
 # Запросить у пользователя цвет фигуры посредством выбора из существующих:
@@ -14,6 +15,49 @@ import simple_draw as sd
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-# TODO здесь ваш код
+def draw_rectangle(start_point, edge_length, start_angle=0, apex_number=3):
+    end_point = start_point
+    for angle in range(start_angle, start_angle + 360 - int(360 / apex_number), int(360 / apex_number)):
+        v1 = sd.get_vector(start_point=end_point, angle=angle, length=edge_length)
+        v1.draw(width=3, color=color)
+        end_point = v1.end_point
+    sd.line(start_point, end_point, width=3, color=color)
+
+
+def draw_triangle(start_point, edge_length, start_angle=0):
+    draw_rectangle(start_point, edge_length, start_angle=0, apex_number=3)
+
+
+def draw_square(start_point, edge_length, start_angle=0):
+    draw_rectangle(start_point, edge_length, start_angle=0, apex_number=4)
+
+
+def draw_pentagon(start_point, edge_length, start_angle=0):
+    draw_rectangle(start_point, edge_length, start_angle=0, apex_number=5)
+
+
+def draw_hexagon(start_point, edge_length, start_angle=0):
+    draw_rectangle(start_point, edge_length, start_angle=0, apex_number=6)
+
+
+colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+
+print('Выберите цвет фигур:')
+print('1) Красный; 2) Оранжевый; 3) желтый; 4) Зелёный; 5) Циан; 6) Синий; 7) Фиолетовый')
+number_input = int(input())
+color = colors[number_input - 1] if 0 < number_input < 8 else None
+
+if color:
+    x = list(range(50, 600, 150))
+    point_0 = sd.get_point(x[0], 200)
+    draw_triangle(point_0, 100, 30)
+    point_0 = sd.get_point(x[1], 200)
+    draw_square(point_0, 80, 60)
+    point_0 = sd.get_point(x[2], 200)
+    draw_pentagon(point_0, 60, 90)
+    point_0 = sd.get_point(x[3], 200)
+    draw_hexagon(point_0, 60, 120)
+else:
+    print('Ошибка выбора цвета')
 
 sd.pause()
--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -2,6 +2,10 @@
 
 import simple_draw as sd
 
+point_0 = sd.get_point(300, 0)
+
+sd.resolution = (1000, 900)
+
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
 # - точка начала рисования,
@@ -9,15 +13,36 @@ import simple_draw as sd
 # - длина ветвей,
 # Отклонение ветвей от угла рисования принять 30 градусов,
 
+# def draw_branches(start_point, angle_draw, branches_length):
+#     delta_angle = 30
+#     vector_1 = sd.get_vector(start_point, angle_draw + delta_angle, branches_length)
+#     vector_2 = sd.get_vector(start_point, angle_draw - delta_angle, branches_length)
+#     vector_1.draw(width=2)
+#     vector_2.draw(width=2)
+
+
 # 2) Сделать draw_branches рекурсивной
 # - добавить проверку на длину ветвей, если длина меньше 10 - не рисовать
 # - вызывать саму себя 2 раза из точек-концов нарисованных ветвей,
 #   с параметром "угол рисования" равным углу только что нарисованной ветви,
 #   и параметром "длинна ветвей" в 0.75 меньшей чем длина только что нарисованной ветви
 
-# 3) первоначальный вызов:
-# root_point = get_point(300, 30)
-# draw_bunches(start_point=root_point, angle=90, length=100)
+# def draw_branches(start_point, angle_draw, branches_length):
+#     if branches_length > 3:
+#         delta_angle = 30
+#         angle_vector_1 = angle_draw + delta_angle
+#         angle_vector_2 = angle_draw - delta_angle
+#         vector = sd.get_vector(start_point,angle_draw, branches_length)
+#         vector.draw(width=2)
+#         draw_branches(vector.end_point, angle_vector_1, branches_length*.75)
+#         draw_branches(vector.end_point, angle_vector_2, branches_length*.75)
+#
+#
+# # 3) первоначальный вызов:
+root_point = sd.get_point(500, 30)
+
+
+# draw_branches(start_point=root_point, angle_draw=90, branches_length=150)
 
 # Пригодятся функции
 # sd.get_point()
@@ -26,16 +51,32 @@ import simple_draw as sd
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
 # - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75
 # Возможный результат решения см lesson_004/results/exercise_04_fractal_02.jpg
 
+def draw_branches(start_point, angle_draw, branches_length):
+    if branches_length > 3:
+        delta_angle = 30
+        angle_vector_1 = angle_draw + delta_angle * sd.randint(60, 140) / 100
+        angle_vector_2 = angle_draw - delta_angle * sd.randint(60, 140) / 100
+        vector = sd.get_vector(start_point, angle_draw, branches_length)
+        vector.draw(width=2)
+        draw_branches(vector.end_point, angle_vector_1, branches_length * .70 * sd.randint(80, 120) / 100)
+        draw_branches(vector.end_point, angle_vector_2, branches_length * .70 * sd.randint(80, 120) / 100)
+    else:
+        delta_angle = 30
+        angle_vector_1 = angle_draw + delta_angle * sd.randint(60, 140) / 100
+        angle_vector_2 = angle_draw - delta_angle * sd.randint(60, 140) / 100
+        vector = sd.get_vector(start_point, angle_draw, branches_length)
+        vector.draw(width=5, color=sd.COLOR_DARK_GREEN)
+
+
+draw_branches(root_point, 90, 100)
+
 # Пригодятся функции
 # sd.random_number()
 
 sd.pause()
-
-
