3826ca32363ba85fea5096d31e86de9188b44a74
Миша <misha.safin.sn0wled@gmail.com>
2021-08-21 14:31:10 +0500

    Написал модуль добавления ингредиентов в бургер Написал код с рецептом бургера

--- lesson_005/03_burger.py
+++ lesson_005/03_burger.py
@@ -15,4 +15,14 @@
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
 
-# TODO здесь ваш код
+import my_burger
+
+my_burger.add_buns()
+my_burger.add_mayo()
+my_burger.add_cheese()
+my_burger.add_pickles()
+my_burger.add_burger()
+my_burger.add_cheese()
+my_burger.add_tomato()
+my_burger.add_mayo()
+my_burger.add_buns()
---
+++ lesson_005/my_burger.py
@@ -0,0 +1,26 @@
+def add_ingredient(ingredient):
+    print(f"А теперь добавим {ingredient}")
+
+
+def add_buns():
+    add_ingredient('булочки')
+
+
+def add_burger():
+    add_ingredient('котлету')
+
+
+def add_pickles():
+    add_ingredient('огурчики')
+
+
+def add_tomato():
+    add_ingredient("помидорки")
+
+
+def add_mayo():
+    add_ingredient('майонез')
+
+
+def add_cheese():
+    add_ingredient('сыр')
